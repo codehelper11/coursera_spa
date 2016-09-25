a526745466be240f14f98c68b4e2f653e643ce6e
vinay <codehelper11@gmail.com>
2016-09-25 22:20:32 +0530

    week2 assignement

---
+++ module2_sol/app.js
@@ -0,0 +1,60 @@
+(function(){
+
+    'use strict';
+    angular.module("mod2",[])
+        .controller("ToBuyShoppingController",ToBuyShoppingController)
+        .controller("AlreadyBoughtShoppingController",AlreadyBoughtShoppingController)
+        .service("ShoppingListCheckOffService",ShoppingListCheckOffService);
+
+    ToBuyShoppingController.$inject=['$scope','ShoppingListCheckOffService'];
+
+    function ToBuyShoppingController($scope,ShoppingListCheckOffService){
+
+        $scope.toBuyList = ShoppingListCheckOffService.getBuyList();
+
+        $scope.moveToBoughtList = function(index){
+            ShoppingListCheckOffService.removeFromBuyAndAddToBoughtList(index);
+
+        }
+    }
+
+    AlreadyBoughtShoppingController.$inject = ['$scope','ShoppingListCheckOffService'];
+
+    function AlreadyBoughtShoppingController($scope,ShoppingListCheckOffService){
+
+        $scope.boughtList = ShoppingListCheckOffService.getBoughtList();
+
+    }
+
+    function ShoppingListCheckOffService (){
+
+        var service = this;
+
+        var toBuyList = [
+            {name: "cookies", quantity: 10},
+            {name: "choc", quantity: 1},
+            {name: "candy", quantity: 2},
+            {name: "icecream", quantity: 3},
+            {name: "milk choc", quantity: 4}
+        ];
+
+        var boughtList = [];
+
+        service.getBuyList = function(){
+            return toBuyList;
+        };
+
+        service.removeFromBuyAndAddToBoughtList = function(index){
+            boughtList.push(toBuyList[index]);
+            toBuyList.splice(index,1);
+
+
+        };
+
+        service.getBoughtList = function(){
+            return boughtList;
+        }
+
+    }
+
+})();
